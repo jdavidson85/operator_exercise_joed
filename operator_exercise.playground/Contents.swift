4121217858cbf0c78ca426188d2b88c4ff36e68a
Joe Davidson <jdavidson@students.mchenry.edu>
2024-02-06 09:26:28 -0600

    Add files via upload

---
+++ operator_exercise.playground/Contents.swift
@@ -0,0 +1,19 @@
+import Foundation
+ 
+// Input Variables
+let num1: Double = 10
+let num2: Double = 5
+ 
+// Operations
+let addition = num1 + num2
+let subtraction = num1 - num2
+let multiplication = num1 * num2
+let division = num1 / num2
+let percentage = (num1 / num2) * 100
+ 
+// Results
+print("Addition: \(addition)")
+print("Subtraction: \(subtraction)")
+print("Multiplication: \(multiplication)")
+print("Division: \(division)")
+print("Percentage: \(percentage)%")
